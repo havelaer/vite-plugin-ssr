7d9fb5de2d54c926e8cb9aa68c7cad733d6589c5
Max Meinders <max@havelaer.nl>
2025-10-03 09:58:50 +0200

    chore: update basic example

--- examples/basic/server.js
+++ examples/basic/server.js
@@ -1,7 +1,7 @@
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
-import { api, ssr } from "./dist/index.js";
+import { api, ssr, ssrContext } from "./dist/index.js";
 
 const app = new Hono();
 
@@ -9,7 +9,7 @@ app.use(serveStatic({ root: "./dist/client" }));
 
 app.use("/api/*", (c) => api(c.req.raw));
 
-app.use((c) => ssr(c.req.raw));
+app.use((c) => ssr(c.req.raw, ssrContext));
 
 serve(app, (info) => {
   console.log(`Listening on http://localhost:${info.port}`);
--- examples/basic/src/entry-ssr.ts
+++ examples/basic/src/entry-ssr.ts
@@ -1,5 +1,17 @@
 import type { Context } from "@havelaer/vite-plugin-ssr";
 
+const scripts = (js: Context["assets"]["js"]) =>
+  js
+    .map((js: any) =>
+      "path" in js
+        ? `<script src="${js.path}" type="module"></script>`
+        : `<script type="module">${js.content}</script>`,
+    )
+    .join("\n");
+
+const styles = (css: Context["assets"]["css"]) =>
+  css.map((css: any) => `<link rel="stylesheet" href="${css.path}" />`).join("\n");
+
 export default async function fetch(request: Request, ctx: Context<"api">): Promise<Response> {
   try {
     // Create an API Request
@@ -12,27 +24,14 @@ export default async function fetch(request: Request, ctx: Context<"api">): Prom
     const apiResponse = await ctx.apis.api(apiRequest).then((r: any) => r.json());
 
     return new Response(
-      `
-      ${ctx.assets.css
-        .map((css) =>
-          "path" in css
-            ? `<link rel="stylesheet" href="${css.path}" />`
-            : `<style>${css.content}</style>`,
-        )
-        .join("\n")}
+      `${styles(ctx.assets.css)}
   
       <h1>Rendered on server</h1>
       <div id="app"></div>
       <div id="api"></div>
       <div><h1>Fetched from server: ${apiResponse.message}</h1></div>
   
-      ${ctx.assets.js
-        .map((js) =>
-          "path" in js
-            ? `<script src="${js.path}" type="module"></script>`
-            : `<script type="module">${js.content}</script>`,
-        )
-        .join("\n")}
+      ${scripts(ctx.assets.js)}
     `,
       {
         headers: {
